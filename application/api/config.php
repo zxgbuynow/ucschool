7f217c9adf5064e5db7afc5da00886df3b442742
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-05-09 15:39:50 +0800

    []

--- application/api/config.php
+++ application/api/config.php
@@ -3,15 +3,56 @@
 
 return [
     'api'=>[
+        //登录
         'login'=>'user.login',//登录
         'register'=>'user.register',//注册
         'findPassword'=>'user.findPassword',
         'sendSms'=>'user.sendSms',
         'getUserInfo'=>'user.getUserInfo',
+        //发现
         'findSearch'=>'find.findSearch',
         'getFocusCollege'=>'find.getFocusCollege',
         'changeCollege'=>'find.changeCollege',
         'getCollegeInfo'=>'find.getCollegeInfo',
-        'focusCollege'=>'find.focusCollege'
+        'focusCollege'=>'find.focusCollege',
+        //课程
+        'getTodayCourse'=>'class.getTodayCourse',
+        'getMyCourse'=>'class.getMyCourse',
+        'getCourseCollege'=>'class.getCourseCollege',
+        'getCourseType'=>'class.getCourseType',
+        'addlessons'=>'class.addlessons',
+
+        'getRecordedCourseList'=>'class.getRecordedCourseList',
+        'getRecordedLessonsList'=>'class.getRecordedLessonsList',
+        'saveCourse'=>'class.saveCourse',
+        'deleteCourse'=>'class.deleteCourse',
+        'publishCourse'=>'class.publishCourse',
+
+        'getUploadCoursewareList'=>'class.getUploadCoursewareList',
+        'saveWeike'=>'class.saveWeike',
+        'addweikelessons'=>'class.addweikelessons',
+        'getpaynumberlist'=>'class.getpaynumberlist',
+        'getCoursewaredataList'=>'class.getCoursewaredataList',
+
+        'getExamList'=>'class.getExamList',
+        'submitExamList'=>'class.submitExamList',
+        'getlearningrecord'=>'class.getlearningrecord',
+        'getCourseEvaluation'=>'class.getCourseEvaluation',
+        'getCourse'=>'class.getCourse',
+        //U学院
+        'ContactList'=>'class.ContactList',
+        'FriendApplicationList'=>'class.FriendApplicationList',
+        'SearchForFriends'=>'class.SearchForFriends',
+        'AddFriends'=>'class.AddFriends',
+        'LaunchAGroupChat'=>'class.LaunchAGroupChat',
+
+        'GroupList'=>'class.GroupList',
+        'ModifyingPersonalInformation'=>'class.ModifyingPersonalInformation',
+        'BusinessCard'=>'class.BusinessCard',
+        'ClassroomSetting'=>'class.ClassroomSetting',
+        'ResourceLibrary'=>'class.ResourceLibrary',
+
+        'PaymentDetails'=>'class.PaymentDetails',
+        'surplusDetails'=>'class.surplusDetails'
     ]
 ];
--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -274,7 +274,7 @@ class Index
         // }
         
         //短信
-        $code = $this->sendmsg($phone);
+        $code = $this->sendmsg($phone, $type);
         if (!$code) {
             return $this->error('发送失败，1小时只能获得3次');
         }
@@ -434,17 +434,41 @@ class Index
             return $this->error('该学院不存在或删除');
         }
 
+        //处理返回
+
+        $rs = array();
+        $rs['collegeid'] = $ret['school_id'];
+        $rs['title'] = $ret['school_name'];
+        $rs['introduce'] = $ret['school_profile'];
+
+        $rs['achievement'] = array();
+        $rs['achtitle'] = '';
+        $rs['achdesc'] = '';
+        $rs['achimages'] = '';
+        $rs['number'] = $ret['school_name'];
+        $rs['isfocus'] = 0;
+
+        $amap['type'] = 2;
+        $amap['del'] = 0;
+        $amap['source_id'] = $ret['school_id'];
+        if (db('toplearning_attention')->where($amap)->find()) {
+            $rs['isfocus'] = 1;
+        }
         //返回信息
         $data = [
             'Code'=>'0',
             'Msg'=>'操作成功',
-            'Data'=>$ret,
+            'Data'=>$rs,
             'Success'=>true
         ];
 
         return json($data);
     }
-
+    /**
+     * [focusCollege 关注]
+     * @param  [type] $params [description]
+     * @return [type]         [description]
+     */
     public function focusCollege($params)
     {
         //params
@@ -518,44 +542,116 @@ class Index
         return json($data);
     }
 
+    /**
+     * [getTodayCourse 今日课程]
+     * @param  [type] $params [description]
+     * @return [type]         [description]
+     */
+    public function getTodayCourse($params)
+    {
+
+        $ret = array();
+        //返回信息
+        $data = [
+            'Code'=>'0',
+            'Msg'=>'操作成功',
+            'Data'=>1,
+            'Success'=>true
+        ];
+
+        return json($data);
+    }
     //---------- common function-----------
+    /**
+     * 发送
+     */
+    public function sendmsg($mobile,$type)
+    {
+        $apikey = '071233bb9140590d2dc4e2b8d4a77d90'; 
+        $getback = '1349539';
+        $regist = '1349531';
+        
+        $code  = rand(1000,9999);
+        $ch = curl_init();
+        
+        /* 设置验证方式 */
+        
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:text/plain;charset=utf-8', 'Content-Type:application/x-www-form-urlencoded','charset=utf-8'));
+        
+        /* 设置返回结果为流 */
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        /* 设置超时时间*/
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        
+        /* 设置通信方式 */
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+
+        $data=array('tpl_id'=>$type==1?$getback:$regist,'tpl_value'=>('#code#').'='.urlencode($code),'apikey'=>$apikey,'mobile'=>$mobile);
+        $json_data = $this->tpl_send($ch,$data);
+        $array = json_decode($json_data,true);
+     // echo '<pre>';print_r($array);
+        
+        curl_close($ch);
+        if ($array['code']==0) {
+            return $code;
+        }else{
+            return false;
+        }
+        //打印获得的数据
+        return $json_data;
+        
+    }
+
     /**
      * [sendmsg description]
      * @param  [type] $mobile [description]
      * @return [type]         [description]
      */
-    public function sendmsg($mobile)
-    {
-        $apikey = "8df6ed7129c50581eecdf1e875edbaa3"; 
+    // public function sendmsg($mobile)
+    // {
+    //     $apikey = "8df6ed7129c50581eecdf1e875edbaa3"; 
 
-        $code  = rand(1000,9999);
-        $text="【希望24热线】您的验证码是".$code; 
+    //     $code  = rand(1000,9999);
+    //     $text="【希望24热线】您的验证码是".$code; 
 
-        $ch = curl_init();
+    //     $ch = curl_init();
  
-         /* 设置验证方式 */
-         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:text/plain;charset=utf-8',
-             'Content-Type:application/x-www-form-urlencoded', 'charset=utf-8'));
-         /* 设置返回结果为流 */
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //      /* 设置验证方式 */
+    //      curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:text/plain;charset=utf-8',
+    //          'Content-Type:application/x-www-form-urlencoded', 'charset=utf-8'));
+    //      /* 设置返回结果为流 */
+    //      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
          
-         /* 设置超时时间*/
-         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    //       设置超时时间
+    //      curl_setopt($ch, CURLOPT_TIMEOUT, 10);
          
-         /* 设置通信方式 */
-         curl_setopt($ch, CURLOPT_POST, 1);
-         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    //      /* 设置通信方式 */
+    //      curl_setopt($ch, CURLOPT_POST, 1);
+    //      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
          
-         // 发送短信
-         $data = array('text'=>$text,'apikey'=>$apikey,'mobile'=>$mobile);
-         $json_data = $this->send($ch,$data);
-         $array = json_decode($json_data,true);  
-         if ($array['code']==0) {
-            return $code;
-         }else{
-            return false;
-         }
+    //      // 发送短信
+    //      $data = array('text'=>$text,'apikey'=>$apikey,'mobile'=>$mobile);
+    //      $json_data = $this->send($ch,$data);
+    //      $array = json_decode($json_data,true);  
+    //      if ($array['code']==0) {
+    //         return $code;
+    //      }else{
+    //         return false;
+    //      }
+    // }
+
+    /**
+     * @return 模板发送  json
+     */
+    function tpl_send($ch,$data){
+        curl_setopt ($ch, CURLOPT_URL, 'https://sms.yunpian.com/v2/sms/tpl_single_send.json');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        return curl_exec($ch);
     }
+
     /**
      * [send description]
      * @param  [type] $ch   [description]
