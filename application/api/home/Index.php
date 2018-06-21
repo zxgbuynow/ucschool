0b07683118aa8bd6d2cdd0e5735c50c826622a25
zhouxiaogong <zhouxiaogong@shopex.cn>
2018-06-21 17:26:53 +0800

    324

--- application/api/home/Index.php
+++ application/api/home/Index.php
@@ -3589,7 +3589,7 @@ class Index
                
             }
             @$ret[$key]['msg'] = $lastmsg;
-
+            $ret[$key]['num'] = $value['MemberNum'];
             $ret[$key]['data'] = date("m月d日",$value['LastMsgTime']);
             $ret[$key]['unreadMsgNumber'] = $value['SelfInfo']['UnreadMsgNum'];
             
@@ -3631,7 +3631,7 @@ class Index
         $info = db('toplearning_chat_group_user')->alias('a')->join('toplearning_login r','a.user_id = r.user_id')->join('toplearning_chat_group g','a.group_id = g.id')->where(['g.txgroupid'=>$groupId])->select();
         $ret = [];
         foreach ($info as $key => $value) {
-            $ret[$key]['name'] = $value['nickname'];
+            $ret[$key]['name'] = $value['nickname']?$value['nickname']:$value['mobile'];
             $ret[$key]['head'] = $value['avatar'];
         }
         //返回信息
